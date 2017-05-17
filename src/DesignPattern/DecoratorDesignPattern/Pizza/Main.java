dbb810738c2e72621e164c2356a04742adc53afb
Nikhil.Agrawal <nikhil.agrawal@target.com>
2017-05-17 23:55:13 +0530

    adding readme.md in decorator design pattern

--- src/DesignPattern/DecoratorDesignPattern/Pizza/Main.java
+++ src/DesignPattern/DecoratorDesignPattern/Pizza/Main.java
@@ -7,14 +7,17 @@ public class Main {
     public static void main(String[] args) 
     {
         
-        BasicPizza basicPizza = new BasicPizza();     
+//        BasicPizza basicPizza = new BasicPizza();
         
-        Pizza chickenpizza = new ChickenTikkaPizza(basicPizza);
+        Pizza chickenpizza = new ChickenTikkaPizza(new BasicPizza());
         System.out.println(chickenpizza.serve(9451));
          System.out.println(chickenpizza.bakePizza());
  
-        Pizza vegCheesePizza = new VegCheesePizza(basicPizza);
+        Pizza vegCheesePizza = new VegCheesePizza(new BasicPizza());
         System.out.println(vegCheesePizza.serve(0007));
+
+
+        // Please note that following is a
     }
 
 }
---
+++ src/DesignPattern/DecoratorDesignPattern/README.md
@@ -0,0 +1,6 @@
+# Decorator Desgin pattern
+
+Please note that following is also a case of decrator design pattern that we use in everyday life
+
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+Source - https://youtu.be/e4MT_OguDKg?t=1080
